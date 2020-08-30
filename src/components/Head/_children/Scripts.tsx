641f1ade966184b14d30dea0f8fdc2794146bc91
susomejias <jesusmejias.jm@gmail.com>
2020-08-30 20:58:35 +0200

    fix build

--- src/components/Head/_children/Scripts.tsx
+++ src/components/Head/_children/Scripts.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 import Head from 'next/head'
 
 const Scripts = () => {
-  return <Head></Head>
+  return (
+    // <Head></Head>
+    <></>
+  )
 }
 
 export default Scripts
