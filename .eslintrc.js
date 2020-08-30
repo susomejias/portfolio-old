1107ed4353d76de2c0a6b71cd70fa77bd2f25ae8
susomejias <jesusmejias.jm@gmail.com>
2020-08-30 20:52:11 +0200

    add jest configuration and first test

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,8 @@ module.exports = {
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  env: {
+    jest: true
   }
 }
