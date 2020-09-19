04e54687f9673817b65e0b5a86f225f9e2e3e03d
susomejias <jesusmejias.jm@gmail.com>
2020-09-19 12:18:35 +0200

    update scripts bash file

--- scripts.sh
+++ scripts.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-npm run test && npm run lint && npm run build && npm run make-badges && npm run lighthouse-badges
+BGreen='\033[1;32m'
+BYellow='\033[1;33m'
+
+echo -e "${BYellow}STARTING CHECKS\n\n"
+
+npm run test &&
+npm run lint &&
+npm run build &&
+npm run make-badges &&
+npm run lighthouse-badges &&
+
+echo -e "${BGreen}\n\nCHECKS PASSED SUCCESSFULLY"
