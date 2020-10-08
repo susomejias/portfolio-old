c0af6f543372a934fe4f5e103040582a2f295778
Miguel Ángel Gavilán Merino <miangame1@gmail.com>
2020-10-08 09:26:00 +0200

    refactor: remove unnecessary useState (#10)

--- src/components/NavBar/_children/ButtonToggleDarkMode.tsx
+++ src/components/NavBar/_children/ButtonToggleDarkMode.tsx
@@ -1,17 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 import useDarkMode from '../../../hooks/useDarkMode'
 
 const ButtonToggleDarkMode = () => {
-  const [iconClassName, setIconClassName] = useState('fas fa-tint')
-
   const { isDarkMode, setDarkMode, setLightMode } = useDarkMode()
-
-  useEffect(() => {
-    isDarkMode
-      ? setIconClassName('fas fa-tint-slash')
-      : setIconClassName('fas fa-tint')
-  }, [isDarkMode])
 
   const handleClick = () => {
     isDarkMode ? setLightMode() : setDarkMode()
@@ -19,7 +11,7 @@ const ButtonToggleDarkMode = () => {
 
   return (
     <div className="button-toggle-dark-mode" onClick={handleClick}>
-      <i className={iconClassName}></i>
+      <i className={`fas ${isDarkMode ? 'fa-tint-slash' : 'fa-tint'}`}></i>
     </div>
   )
 }
