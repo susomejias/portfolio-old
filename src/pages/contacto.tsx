619e9661ba6bf492d4edda97a012d344d6d4a024
susomejias <jesusmejias.jm@gmail.com>
2020-10-13 17:48:55 +0200

    style(portfolio): :art: fix width for btn download CV

--- src/pages/contacto.tsx
+++ src/pages/contacto.tsx
@@ -12,11 +12,13 @@ const Contact = (): JSX.Element => {
 
       <h2>Suso Mejías</h2>
 
-      <ButtonBadge
-        text="Descarga mi CV actualizado"
-        url="https://drive.google.com/file/d/1OKxUf3lCbID8JA38T_D3q_WLSvDuizow/view"
-        isTargetBlank={true}
-      />
+      <div className="container_btn-download-cv">
+        <ButtonBadge
+          text="Descarga mi CV actualizado"
+          url="https://drive.google.com/file/d/1OKxUf3lCbID8JA38T_D3q_WLSvDuizow/view"
+          isTargetBlank={true}
+        />
+      </div>
 
       <div className="container_social_networks">
         {socialNetworks.map((socialNetwork: SocialNetwork, index: number) => {
