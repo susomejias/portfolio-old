2e7afdeccebd86dc9d67941ad6c1a2d60d1758a2
susomejias <jesusmejias.jm@gmail.com>
2020-11-01 20:23:28 +0100

    feat(portfolio): :camera_flash: add images dimension checker

--- scripts.sh
+++ scripts.sh
@@ -5,11 +5,13 @@ BYellow='\033[1;33m'
 
 echo -e "${BYellow}STARTING CHECKS\n\n"
 
+cd utils && python check_correct_images_dimensions.py &&
 npm run test &&
 npm run cy:run &&
 npm run lint &&
 npm run build &&
 npm run make-badges &&
 npm run lighthouse-badges &&
+
 
 echo -e "${BGreen}\n\nCHECKS PASSED SUCCESSFULLY"
