a3dc90896c0f7a23db6bd5b106a78eb86ed00047
susomejias <jesusmejias.jm@gmail.com>
2021-06-20 01:22:29 +0200

    test(portfolio): :white_check_mark: renaming unit tests

--- src/components/NavBar/ButtonToggleDarkMode/index.spec.tsx
+++ src/components/NavBar/ButtonToggleDarkMode/index.spec.tsx
@@ -6,18 +6,18 @@ import ButtonToggleDarkMode from './index'
 describe('<ButtonToggleDarkMode>', () => {
   const wrapper = mount(<ButtonToggleDarkMode />)
 
-  it('should show the sun icon when dark mode is deactivated', () => {
+  it('should show the sun icon when initial value of dark mode is false', () => {
     expect(wrapper.find('.animate-hide-sun').length).toEqual(0)
     expect(wrapper.find('.animate-show-moon').length).toEqual(0)
   })
 
-  it('should show the moon icon and hide sun icon when clicked', () => {
+  it('should show the moon icon when dark mode is activated', () => {
     wrapper.find('.button-toggle-dark-mode').simulate('click')
     expect(wrapper.find('.animate-hide-sun').length).toEqual(1)
     expect(wrapper.find('.animate-show-moon').length).toEqual(1)
   })
 
-  it('should show the moon icon and hide sun icon when clicked', () => {
+  it('should show the sun icon when dark mode is deactivated', () => {
     wrapper.find('.button-toggle-dark-mode').simulate('click')
     expect(wrapper.find('.animate-hide-sun').length).toEqual(0)
     expect(wrapper.find('.animate-show-moon').length).toEqual(0)
