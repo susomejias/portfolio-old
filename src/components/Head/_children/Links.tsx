614466374f4ae20dbace3f601629dbada398cd54
susomejias <jesusmejias.jm@gmail.com>
2021-06-20 13:07:59 +0200

    feat(portfolio): :zap: remove weight unused font

--- src/components/Head/_children/Links.tsx
+++ src/components/Head/_children/Links.tsx
@@ -50,7 +50,7 @@ const Links = (): JSX.Element => (
     />
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link
-      href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400;500;600;700;800;900&display=swap"
+      href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@500;600;700;800;900&display=swap"
       rel="stylesheet"
     />
   </Head>
