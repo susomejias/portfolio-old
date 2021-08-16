1e6899b352c99965314659ab7ffacfbd73e22dc0
susomejias <jesusmejias.jm@gmail.com>
2021-08-16 14:40:50 +0200

    fix(portfolio): fix content colors

--- src/components/Head/_children/DefaultMetaTags.tsx
+++ src/components/Head/_children/DefaultMetaTags.tsx
@@ -8,9 +8,9 @@ const DefaultMetaTags = (): JSX.Element => (
       name="viewport"
       content="width=device-width, initial-scale=1.0, maximum-scale=5.0, viewport-fit=cover"
     />
-    <meta name="msapplication-TileColor" content="#c8a3f4" />
+    <meta name="msapplication-TileColor" content="#fbbd03" />
     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-    <meta name="theme-color" content="#c8a3f4" />
+    <meta name="theme-color" content="#fbbd03" />
   </Head>
 )
 
