38f3c6255381b653294dab3a7a1f97ad8a5913d9
susomejias <jesusmejias.jm@gmail.com>
2021-08-22 20:35:03 +0200

    perf(portfolio): hosted Font Awesome in local

--- src/components/Head/_children/Links.tsx
+++ src/components/Head/_children/Links.tsx
@@ -42,12 +42,6 @@ const Links = (): JSX.Element => (
     <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
     <link rel="manifest" href="/manifest.json" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
-      integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
-      crossOrigin="anonymous"
-    />
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link
       href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@500;600;700;800;900&display=swap"
